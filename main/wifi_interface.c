52aecd3d11aeb4d424b7f48f1b4986c0a62bf11c
Welison Almeida <welison.almeida.923@gmail.com>
2020-11-29 18:45:03 -0300

    Add wifi interface to project.

---
+++ main/wifi_interface.c
@@ -0,0 +1,85 @@
+#include "wifi_interface.h"
+
+#include <string.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/event_groups.h"
+#include "esp_system.h"
+#include "esp_wifi.h"
+#include "esp_event.h"
+#include "esp_log.h"
+#include "nvs_flash.h"
+#include "lwip/err.h"
+#include "lwip/sys.h"
+
+#define WIFI_SSID CONFIG_ESP_WIFI_SSID
+#define WIFI_PASSWORD CONFIG_ESP_WIFI_PASSWORD
+#define WIFI_MAXIMUM_RETRY CONFIG_ESP_MAXIMUM_RETRY
+#define TAG "Wifi"
+
+static int retry_count = 0;
+extern xSemaphoreHandle conexaoWifiSemaphore;
+extern xSemaphoreHandle led_semaphore;
+
+static void event_handler(void *arg, esp_event_base_t event_base,
+                          int32_t event_id, void *event_data)
+{
+    if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_START)
+    {
+        esp_wifi_connect();
+    }
+    else if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_DISCONNECTED)
+    {
+        if (retry_count < WIFI_MAXIMUM_RETRY)
+        {
+            esp_wifi_connect();
+            retry_count++;
+            ESP_LOGI(TAG, "Houve um problema ao conectar na rede %s. Tentando novamente...", WIFI_SSID);
+        }
+        else
+        {
+            wifi_stop();
+            ESP_LOGE(TAG, "Limite de conexões excedido. Verifique a rede %s e reinicie o dispositivo.", WIFI_SSID);
+        }
+    }
+    else if (event_base == IP_EVENT && event_id == IP_EVENT_STA_GOT_IP)
+    {
+        ip_event_got_ip_t *event = (ip_event_got_ip_t *)event_data;
+        ESP_LOGI(TAG, "Conectado a rede %s (senha %s) no endereço IP: " IPSTR,
+                 WIFI_SSID, WIFI_PASSWORD, IP2STR(&event->ip_info.ip));
+        xSemaphoreGive(conexaoWifiSemaphore);
+        xSemaphoreTake(led_semaphore, portMAX_DELAY);
+        retry_count = 0;
+    }
+}
+
+void wifi_start()
+{
+    ESP_ERROR_CHECK(esp_netif_init());
+
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
+    esp_netif_create_default_wifi_sta();
+
+    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+    ESP_ERROR_CHECK(esp_wifi_init(&cfg));
+
+    ESP_ERROR_CHECK(esp_event_handler_register(WIFI_EVENT, ESP_EVENT_ANY_ID, &event_handler, NULL));
+    ESP_ERROR_CHECK(esp_event_handler_register(IP_EVENT, IP_EVENT_STA_GOT_IP, &event_handler, NULL));
+
+    wifi_config_t wifi_config = {
+        .sta = {
+            .ssid = WIFI_SSID,
+            .password = WIFI_PASSWORD},
+    };
+
+    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
+    ESP_ERROR_CHECK(esp_wifi_set_config(ESP_IF_WIFI_STA, &wifi_config));
+    ESP_ERROR_CHECK(esp_wifi_start());
+}
+
+void wifi_stop()
+{
+    ESP_ERROR_CHECK(esp_event_handler_unregister(IP_EVENT, IP_EVENT_STA_GOT_IP, &event_handler));
+    ESP_ERROR_CHECK(esp_event_handler_unregister(WIFI_EVENT, ESP_EVENT_ANY_ID, &event_handler));
+    xSemaphoreGive(led_semaphore);
+}
